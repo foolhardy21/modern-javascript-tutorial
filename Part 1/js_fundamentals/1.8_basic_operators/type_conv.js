38b77906a201b56c12c92f909f02a7facc823842
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-04-28 18:53:33 +0530

    operators_maths

---
+++ Part 1/js_fundamentals/1.8_basic_operators/type_conv.js
@@ -0,0 +1,7 @@
+console.log('""+1+0',""+1+0)
+console.log('""-1+0',""-1+0)
+console.log('true+false',true+false)
+console.log('6/"3"',6/"3")
+console.log('"2"*"3"',"2"*"3")
+console.log('null+1',null+1)
+console.log('undefined+1',undefined+1)
