083b01989960a389e9e08a4e1386f27b4a2cfe76
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-04-30 12:31:50 +0530

    1.9_comparators

---
+++ Part 1/js_fundamentals/1.9_comparisons.js
@@ -0,0 +1,8 @@
+console.log(5>4)
+console.log("apple">"pineapple") //lexicographical comparison
+console.log("2">"12")
+console.log(undefined == null) //undefined is incomaprable
+console.log(undefined === null)
+console.log(null === "\n0\n")
+console.log(null === +"\n0\n")
+console.log(null >= +"\n0\n")//>= converts null to 0
