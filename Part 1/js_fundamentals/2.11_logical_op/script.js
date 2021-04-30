520c3ec11cc6056bfb45b8822093c130f7a3c1e3
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-04-30 15:16:03 +0530

    check-the-login

--- Part 1/js_fundamentals/2.11_logical_op/script.js
+++ Part 1/js_fundamentals/2.11_logical_op/script.js
@@ -1,3 +1,16 @@
-if(-1 || 0) alert('first')
-if(-1 && 0) alert('second')
-if(null || -1 && 0) alert('third')
+let login = prompt('Who is there?')
+
+if(login === 'Admin'){
+  let pass = prompt('Password?')
+  if(pass === 'TheMaster')
+  alert('Welcome')
+  else if(pass === '' || pass === null)
+  alert('Cancelled')
+  else
+  alert('Wrong Password')
+
+}
+else if(login === '' || login === null)
+alert('Cancelled')
+else
+alert('I dont know you')
