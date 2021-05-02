1487d26898b55ddb5527df1fccc9d13ebec77b45
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-05-02 20:34:06 +0530

    check-for-emptiness

--- Part 1/objects/4.1_object_basics.js
+++ Part 1/objects/4.1_object_basics.js
@@ -1,14 +1,16 @@
-user = {}
+let schedule ={}
 
-function printObject() {
-  console.log(user)
+console.log( isEmpty(schedule) )
+
+schedule["8:30"] = "get up"
+
+console.log( isEmpty(schedule) )
+
+function isEmpty(schedule) {
+  var counter = 0
+
+  for(var key in schedule){
+    counter++;
+  }
+  return counter === 0 ? true : false
 }
-printObject()
-user['name'] = 'John'
-printObject()
-user['surname'] = 'Smith'
-printObject()
-user['name'] = 'Pete'
-printObject()
-delete user.name
-printObject()
