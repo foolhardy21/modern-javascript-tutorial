7e187bc44c8271c7cb0eef305b7f230c1231f222
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-23 21:49:43 +0530

    part-2 - 2.1-2

--- Part 2/intro-to-events/2.1-introduction/main.js
+++ Part 2/intro-to-events/2.1-introduction/main.js
@@ -1,8 +1,7 @@
 const button = document.querySelector('#hider')
-const text = document.querySelector('#text')
 
 button.addEventListener('click',hideText)
 
 function hideText(e) {
-  text.style.display='none'
+  button.style.display='none'
 }
